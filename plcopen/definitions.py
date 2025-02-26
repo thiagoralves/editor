cfe04e82f98ddcd401ee60bce5f13773b8671346
Michael Scott <soulcrusher842@gmail.com>
2025-02-26 14:05:29 -0500

    adds synergy modules fb

--- plcopen/definitions.py
+++ plcopen/definitions.py
@@ -53,6 +53,7 @@ StdTC6Libs = [(_("Standard function blocks"),   join(sd, "Standard_Function_Bloc
               (_("Microver"), join(sd, "CAN_Function_Blocks.xml")),
               (_("Communication"), join(sd, "Communication_Blocks.xml")),
               (_("P1AM Modules"), join(sd, "P1AM.xml")),
+              (_("Synergy Logic Modules"), join(sd, "Synergy.xml")),
               (_("MQTT"), join(sd, "MQTT.xml")),
               (_("Sequent Microsystems Modules"), join(sd, "SM_Cards.xml")),
               (_("Jaguar"), join(sd, "Jaguar.xml")),
